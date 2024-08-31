f928da84af6cd20ff7e64d1d131e869254f28675
Justin Morrison <morrisonxjustin@gmail.com>
2024-08-30 21:38:41 -0400

    Added new functions from Terminal. Fixed a lot of issues to do with unwanted text getting deleted. Much improved the user experience. Added timings to things printing to make it look better. Also, unneeded text gets deleted. Fixed some spacing issues with the headers printing. Added exit clause to get_filtering_preference.

--- CGPA_Calculator.py
+++ CGPA_Calculator.py
@@ -1,7 +1,8 @@
 import csv
+
 from Course import Course, Semester
 from CourseList import CourseList
-from Terminal import move_cursor_up, move_cursor_down, clear_line, clear_previous_line
+from Terminal import move_cursor_up, move_cursor_down, clear_line, clear_previous_line_back, clear_previous_lines, print_sleep
 
 
 def main():
@@ -10,24 +11,33 @@ def main():
 
     filter_preference = get_filtering_preference()
 
-    if filter_preference == "both":
-        semester = get_semester()
-        year = get_year(semester)
-        filtered_courses = course_list.filter_by_year_and_semester(year, semester)
+    if filter_preference == "year":
+        year = get_year()
+        clear_previous_lines(1)
 
-    elif filter_preference == "year":
-        year = get_year(None)
+        print(f"{year} Classes:")
         filtered_courses = course_list.filter_by_year(year)
 
     elif filter_preference == "semester":
         semester = get_semester()
+        clear_previous_lines(1)
+
+        print(f"{semester.title()} Classes:")
         filtered_courses = course_list.filter_by_semester(semester)
+
+    elif filter_preference == "both":
+        semester = get_semester()
+        year = get_year(semester)
+        clear_previous_lines(2)
+
+        print(f"{semester.title()} {year} Classes:")
+        filtered_courses = course_list.filter_by_year_and_semester(year, semester)
 
     elif filter_preference == "all":
         filtered_courses = course_list
 
-    print(filtered_courses.headers)
-    print(f"\n{filtered_courses}")
+    print(f"\n{filtered_courses.headers}\n")
+    print(f"{filtered_courses}")
 
     calc_cgpa(filtered_courses, check_major=True)
 
@@ -46,16 +56,17 @@ def get_filtering_preference() -> str:
     print("2. View all semesters of a certain type across different years")
     print("3. View courses from a single semester in a single year")
     print("4. View all classes")
-    choice = input("Enter 1, 2, 3, or 4: ").strip()
+    choice = input("\nEnter 1, 2, 3, or 4: ").strip()
 
     while choice not in ["1", "2", "3", "4"]:
-        move_cursor_up()
-        clear_line()
-        move_cursor_up()
-        print("Please enter 1, 2, 3, 4.")
-        choice = input("Enter 1, 2, 3, or 4: ").strip()
+        if choice in ['q', 'Q']:
+            raise SystemExit("Program terminated by user.")
+        else:
+            move_cursor_up()
+            clear_line()
+            choice = input("Enter 1, 2, 3, or 4: ").strip()
 
-    clear_previous_line(2)
+    clear_previous_lines(7)
 
     return options[choice]
 
@@ -66,7 +77,7 @@ def get_semester() -> str:
 
     :return: The semester returned as a string
     """
-    semester = input("\nEnter Semester:  ").title()
+    semester = input("Enter Semester:  ").title()
 
     while semester not in Course.valid_semesters:
         if semester == 'Q':
@@ -79,11 +90,11 @@ def get_semester() -> str:
 
             semester = input("Enter Semester:  ").title()
 
-    clear_previous_line(2)
+    clear_previous_line_back(2)
     return semester
 
 
-def get_year(semester: str) -> int:
+def get_year(semester: str = None) -> int:
     """
     Prompts user to enter a valid year for the given semester
 
@@ -107,6 +118,8 @@ def get_year(semester: str) -> int:
                 move_cursor_down()
 
                 year = input("Enter Year:      ")
+
+        clear_previous_line_back(3)
     else:
         while year not in valid_years:
             if year in ['q', "Q"]:
@@ -114,14 +127,12 @@ def get_year(semester: str) -> int:
             else:
                 move_cursor_up()
                 clear_line()
-                move_cursor_up(2)
+                move_cursor_up()
 
                 print(f'Please enter any of the following: {valid_years}')
-                move_cursor_down()
-
                 year = input("Enter Year:      ")
 
-    clear_previous_line(3)
+        clear_previous_line_back(2)
 
     return int(year)
 
@@ -153,21 +164,22 @@ def calc_cgpa(courses: CourseList, check_major: bool = False) -> None:
     :param courses: CourseList object containing courses to have calculated
     :param check_major: Boolean whether to include major specific CGPA or not
     """
+    print_delay = 0.3
 
     if courses.total_credits != 0:
-        print(f"\nCredits Earned: {courses.total_credits}")
+        print_sleep(f"Credits Earned: {courses.total_credits}", print_delay)
 
         cgpa = courses.total_grade_points_earned/courses.total_credits
-        print(f"CGPA: {cgpa:.2f}\n")
+        print_sleep(f"CGPA: {cgpa:.2f}\n", print_delay)
     else:
         print("0 Credits Earned. Cannot Calculate CGPA")
 
     if check_major:
         if courses.major_credits != 0:
-            print(f"Major Credits Earned: {courses.major_credits}")
+            print_sleep(f"Major Credits Earned: {courses.major_credits}", print_delay)
 
             major_cgpa = courses.major_grade_points_earned/courses.major_credits
-            print(f"Major CGPA: {major_cgpa:.2f}\n")
+            print_sleep(f"Major CGPA: {major_cgpa:.2f}\n", print_delay)
         else:
             print("0 Major Credits Earned. Cannot Calculate Major CGPA")
 
