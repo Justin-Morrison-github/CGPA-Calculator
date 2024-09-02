8a9d15142cd0e42ddb364ff1b2e35d9e0246b343
Justin Morrison <morrisonxjustin@gmail.com>
2024-09-01 23:52:42 -0400

    Added term_list to Term. Added "UNS" grade to Course.grades. Added grade_colors to determine color of text to use for each grade, colorama used to color text. Also, added color grade text to repr. Added ability for Course to be instantiated with an empty dict, in which case all properties are set to None. The dict arguement is set to None since it is a mutable default arguement.

--- Course.py
+++ Course.py
@@ -1,7 +1,11 @@
+from colorama import Fore
+
+
 class Term():
     FALL = "Fall"
     WINTER = "Winter"
     SUMMER = "Summer"
+    term_list = [FALL, WINTER, SUMMER]
 
 
 class Course():
@@ -19,7 +23,26 @@ class Course():
         "D": 2.0,
         "D-": 1.0,
         "F": 0.0,
-        "SAT": 0.0
+        "SAT": 0.0,
+        "UNS": 0.0
+    }
+
+    grade_colors = {
+        "A+": f"{Fore.GREEN}A+{Fore.RESET}",
+        "A": f"{Fore.GREEN}A{Fore.RESET}",
+        "A-": f"{Fore.GREEN}A-{Fore.RESET}",
+        "B+": f"{Fore.BLUE}B+{Fore.RESET}",
+        "B": f"{Fore.BLUE}B{Fore.RESET}",
+        "B-": f"{Fore.BLUE}B-{Fore.RESET}",
+        "C+": f"{Fore.YELLOW}C+{Fore.RESET}",
+        "C": f"{Fore.YELLOW}C{Fore.RESET}",
+        "C-": f"{Fore.YELLOW}C-{Fore.RESET}",
+        "D+": f"{Fore.RED}D+{Fore.RESET}",
+        "D": f"{Fore.RED}D{Fore.RESET}",
+        "D-": f"{Fore.RED}D-{Fore.RESET}",
+        "F": f"{Fore.RED}F{Fore.RESET}",
+        "SAT": f"{Fore.GREEN}SAT{Fore.RESET}",
+        "UNS": f"{Fore.RED}UNS{Fore.RESET}",
     }
 
     valid_terms = {
@@ -28,33 +51,46 @@ class Course():
         Term.SUMMER: []
     }
 
-    def __init__(self, course_dict: dict):
-        self.name = course_dict['Course Title']
-        self.subject = course_dict['Subject']
-        self.section = course_dict['Section']
-
-        self.year = int(course_dict['Year'])
-        self.CRN = int(course_dict['CRN'])
-        self.course_number = int(course_dict['Course Number'])
-
-        self.credit = float(course_dict['Credit'])
-        self.CGPA_points = float(course_dict['CGPA Points'])
-
-        if course_dict['Term'] in Course.valid_terms:
-            self.term = course_dict['Term']
+    def __init__(self, course_dict: dict = None):
+        if course_dict is None:
+            self.name = None
+            self.subject = None
+            self.section = None
+            self.year = None
+            self.CRN = None
+            self.course_number = None
+            self.credit = None
+            self.CGPA_points = None
+            self.term = None
+            self.grade = None
+            self.in_major = None
         else:
-            raise ValueError(f"Invalid term: {course_dict['Term']}")
+            self.name = course_dict['Course Title']
+            self.subject = course_dict['Subject']
+            self.section = course_dict['Section']
 
-        if course_dict["Final Grade"] in Course.grades:
-            self.grade = course_dict['Final Grade']
-        else:
-            raise ValueError(f"Invalid grade: {course_dict['Final Grade']}")
+            self.year = int(course_dict['Year'])
+            self.CRN = int(course_dict['CRN'])
+            self.course_number = int(course_dict['Course Number'])
 
-        self.in_major = course_dict["Major"].strip().lower() in ['true', '1', 'yes']
+            self.credit = float(course_dict['Credit'])
+            self.CGPA_points = float(course_dict['CGPA Points'])
+
+            if course_dict['Term'] in Course.valid_terms:
+                self.term = course_dict['Term']
+            else:
+                raise ValueError(f"Invalid term: {course_dict['Term']}")
+
+            if course_dict["Final Grade"] in Course.grades:
+                self.grade = course_dict['Final Grade']
+            else:
+                raise ValueError(f"Invalid grade: {course_dict['Final Grade']}")
+
+            self.in_major = course_dict["Major"].strip().lower() in ['true', '1', 'yes']
 
     def __repr__(self) -> str:
         return f"""{
-            self.grade: <8}{
+            Course.grade_colors[self.grade]:<18}{
             self.credit: <10.2f}{
             self.term: <8}{
             self.year: <8}{
